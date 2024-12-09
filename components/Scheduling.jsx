573c6551e85965c9f2377246c2e79bab317e22a1
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-12-09 07:44:39 +0200

    Stuff

--- components/Scheduling.jsx
+++ components/Scheduling.jsx
@@ -4,7 +4,7 @@ import { InlineWidget } from "react-calendly";
 export default function Scheduling() {
   return (
     <InlineWidget
-      styles={{ width: "100%", height: "800px" }}
+      styles={{ width: "100%", height: "1000px" }}
       className=' w-full'
       url='https://calendly.com/valtterijuvonen123/30min'
     />
