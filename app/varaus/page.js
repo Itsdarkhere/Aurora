f79c83625bfd233925fa409f5b4da9bf1b809a23
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-12-09 07:40:28 +0200

    no px for varaus

--- app/varaus/page.js
+++ app/varaus/page.js
@@ -3,7 +3,7 @@ import Scheduling from "@/components/Scheduling";
 
 export default function Varaus() {
   return (
-    <div className="w-full flex flex-col px-4">
+    <div className="w-full flex flex-col">
         <Scheduling />
     </div>
   );
