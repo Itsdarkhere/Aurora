c8581bfeed13bee2328eb126dd151ea0cf66c365
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-12-12 07:41:46 +0200

    Styling...

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -2,20 +2,35 @@ import Link from "next/link";
 
 export default function CTA() {
   return (
-    <div className=' w-full bg-slate-50 md:py-12 md:px-4 flex justify-center items-center'>
-      <div className=' bg-green-800 text-center md:rounded-2xl py-12 px-12 max-w-7xl w-full h-full flex flex-col justify-center items-center'>
-        <h3 className=' text-4xl font-bold text-white mb-4'>
-          Kehitä itseäsi ammattilaisena
-        </h3>
-        <p className=' leading-6 text-lg max-w-2xl text-gray-300  mb-12'>
-          Psyykkinen valmennuksen avulla voit saada apua monenlaisiin
-          haasteisiin ja voimme työskennellä tapaamisilla muun muassa stressin
-          hallinnan ja paineensiedon, vireystilan säätelyn, motivaation,
-          tavoitteiden asettelun ja itseluottamuksen vahvistamisen parissa.{" "}
-        </p>
-        <Link href='/varaus' className=' bg-slate-50 rounded-md text-gray-800 px-6 py-2'>
-          Varaa tapaaminen
-        </Link>
+    <div className=' w-full bg-slate-50 md:py-44 md:px-4 flex justify-center items-center'>
+      <div className='flex flex-row justify-center gap-8 max-w-7xl w-full'>
+      <div className=' w-96 h-96 rounded-xl bg-green-950'></div>
+        <div className=' flex flex-col justify-center items-start'>
+          <h3 className=' text-4xl font-bold text-green-950 mb-4'>
+            Kehitä itseäsi ammattilaisena
+          </h3>
+          <p className=' leading-6 text-lg max-w-2xl text-green-900  mb-12'>
+            Psyykkinen valmennuksen avulla voit saada apua monenlaisiin
+            haasteisiin ja voimme työskennellä tapaamisilla muun muassa stressin
+            hallinnan ja paineensiedon, vireystilan säätelyn, motivaation,
+            tavoitteiden asettelun ja itseluottamuksen vahvistamisen parissa.{" "}
+          </p>
+          <div className='flex flex-col justify-center md:justify-start items-center md:flex-row gap-4'>
+            <Link
+              href='/varaus'
+              className='bg-green-800 min-w-64 md:min-w-0 text-center text-white px-6 py-3 rounded-lg font-medium hover:bg-gray-100 transition-colors'
+            >
+              Varaa Tapaaminen
+            </Link>
+            <Link
+              href='/lue-lisaa'
+              className='border min-w-64 md:min-w-0 text-center border-green-800 text-green-800 px-6 py-3 rounded-lg font-medium hover:bg-white/10 transition-colors'
+            >
+              Lue Lisää
+            </Link>
+          </div>
+        </div>
+      
       </div>
     </div>
   );
--- components/Quote.jsx
+++ components/Quote.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Quote = () => {
   return (
-    <section className="bg-gray-50 px-4 py-44">
-      <div className="max-w-4xl text-green-900 mx-auto px-4 relative">
-        <div className="absolute -top-2 left-0 text-sage-200/50 text-4xl" aria-hidden="true">
-          ★
-        </div>
-        
-        <div className="absolute -top-2 right-0 text-sage-200/50 text-4xl" aria-hidden="true">
-          ❋
-        </div>
-
-        <blockquote className="text-center">
-          <p className="text-lg md:text-xl lg:text-2xl text-sage-700 leading-relaxed max-w-3xl mx-auto">
-          &quot;Tavoitteena on vahvistaa suorituskykyäsi mielenprosessien avulla ja auttaa löytämään tasapaino, joka tukee sekä tavoitteidesi saavuttamisessa että kokonaisvaltaisessa jaksamisessa.&quot;
+    <section className='bg-gray-50 px-4 py-44'>
+      <div className='max-w-4xl text-green-900 mx-auto px-4 flex flex-row gap-6 relative'>
+        <div className=' w-12 h-12 rounded-lg bg-green-950 mb-8'></div>
+        <blockquote className='text-center'>
+          <p className='text-lg lg:text-xl text-sage-700 leading-relaxed max-w-3xl mx-auto'>
+            &quot;Tavoitteena on vahvistaa suorituskykyäsi mielenprosessien
+            avulla ja auttaa löytämään tasapaino, joka tukee sekä tavoitteidesi
+            saavuttamisessa että kokonaisvaltaisessa jaksamisessa.&quot;
           </p>
         </blockquote>
       </div>
