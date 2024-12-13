b497b92ae7ed7e0a5473dffee06f69fc73c359d0
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-12-13 05:27:19 +0200

    reservation imporvement on mobile

--- app/varaus/page.js
+++ app/varaus/page.js
@@ -1,10 +1,9 @@
 import Scheduling from "@/components/Scheduling";
 
-
 export default function Varaus() {
   return (
-    <div className="w-full min-h-screen  bg-gradient-to-r to-[#75A896] from-[#53786B] flex flex-col justify-start items-center bg-slate-100">
-        <Scheduling />
+    <div className='w-full min-h-screen bg-green-50 md:bg-gradient-to-r to-[#75A896] from-[#53786B] flex flex-col justify-center md:justify-start items-center'>
+      <Scheduling />
     </div>
   );
 }
