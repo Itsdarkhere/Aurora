e3f0bdfed435fb64a9149f972bb0f942145b23d7
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-12-13 19:36:31 +0200

    Finito

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -35,7 +35,6 @@ const Nav = () => {
 };
 
 function MobileNav() {
-
   const dialogRef = useRef(null);
 
   const handleOpen = () => {
@@ -80,7 +79,7 @@ function MobileNav() {
           </Link>
           <Link
             href='/varaus'
-            className='bg-green-800 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-colors text-center'
+            className='bg-theme text-white hover:bg-green-900 transition-colors px-4 py-2 rounded-md text-center'
             onClick={handleClose}
           >
             Varaa tapaaminen
