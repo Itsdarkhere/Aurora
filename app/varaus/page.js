8875b3af4a46bf75f7e46b88c3e50e4b48a6c849
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2025-01-02 19:35:32 +0200

    bg lightbg

--- app/varaus/page.js
+++ app/varaus/page.js
@@ -2,7 +2,7 @@ import Scheduling from "@/components/Scheduling";
 
 export default function Varaus() {
   return (
-    <div className='w-full min-h-screen bg-green-50 md:bg-gradient-to-r to-[#75A896] from-[#53786B] flex flex-col justify-start items-center'>
+    <div className='w-full min-h-screen bg-lightbg md:bg-gradient-to-r to-[#75A896] from-[#53786B] flex flex-col justify-start items-center'>
       <Scheduling />
     </div>
   );
