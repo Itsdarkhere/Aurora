b0e3ab14bc602a2a7fb3840aaa2fe0f481c3d67f
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2025-01-02 18:52:38 +0200

    Try designs

--- components/MeetingInfo.jsx
+++ components/MeetingInfo.jsx
@@ -1,7 +1,7 @@
 
 export default function MeetingInfo() {
   return (
-    <div className=' w-full bg-theme py-14 px-4 flex justify-center items-center text-center'>
+    <div className=' w-full bg-theme py-14 py-24 px-4 flex justify-center items-center text-center'>
       <h4 className="text-lg lg:text-xl text-white leading-relaxed max-w-3xl mx-auto">Psyykkinen valmennus pidetään kätevästi etätapaamisina joten voit osallistua riippumatta siitä, missä päin Suomea olet.</h4>
     </div>
   );
