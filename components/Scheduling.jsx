5866321c08a3d6a6f2d6ebc7d4686571a4aabef5
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2025-01-27 16:07:03 +0200

    m

--- components/Scheduling.jsx
+++ components/Scheduling.jsx
@@ -6,7 +6,7 @@ export default function Scheduling() {
     <InlineWidget
       styles={{ width: "100%", height: "1000px", padding: "0 !important" }}
       className=' w-full'
-      url='https://calendly.com/aurora-amanda-huovinen/45min'
+      url='https://calendly.com/aurora-amanda-huovinen/30min'
     />
   );
 }
